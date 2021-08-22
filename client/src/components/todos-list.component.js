0f92605064b88054289a778fb10fa4a3e6e5b1fe
Marcus Saralde <leesharped@gmail.com>
2021-08-22 15:00:43 +0800

    Basic layout & navigation

---
+++ client/src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>List</p>
+            </div>
+        )
+    }
+}
