4e2fc2a7e1a427a4a8415b68d842ef4d38028d97
Marcus Saralde <leesharped@gmail.com>
2021-08-24 10:53:47 +0800

    drastic change

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,29 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+import { Route, Switch, Link } from 'react-router-dom'
+import { TodoList } from './TodoList'
+import { CreateTodo } from './CreateTodo'
+import { EditTodo } from './EditTodo'
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
-import TodosList from "./components/todos-list.component";
-
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">MERN-CRUD ToDo </Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">Todos</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
+    <Switch>
+      <Route exact path="/" component={TodoList}/>
+      <Route path="/edit/:id" component={EditTodo}/>
+      <Route path="/create" component={CreateTodo}/>
+    </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/TodoForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const TodoForm = ({ todo, onSubmit }) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: { text: todo ? todo.text : "" },
+  });
+
+  const submitHandler = handleSubmit((data) => {
+    onSubmit(data)
+  });
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-group">
+        <label htmlFor="text">Text:</label>
+        <input
+          className="form-control"
+          {...register('text')}
+          type="text"
+          name="text"
+          id="text"
+        />
+      </div>
+      <div className="form-group">
+        <button type="submit" className="btn btn-primary">
+          Save Todo
+        </button>
+      </div>
+    </form>
+  );
+};
